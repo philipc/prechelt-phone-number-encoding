b421ca1431c077123b263a3e1a7928e6b0d711a9
Philip Craig <philipjcraig@gmail.com>
2021-07-28 14:05:41 +1000

    Use Vec<u8>

--- src/rust/phone_encoder/src/main.rs
+++ src/rust/phone_encoder/src/main.rs
@@ -4,15 +4,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-use lazy_static::lazy_static;
-use num_bigint::BigUint;
-
-type Dictionary = HashMap<BigUint, Vec<String>>;
-
-lazy_static! {
-    static ref ONE: BigUint = 1u8.into();
-    static ref TEN: BigUint = 10u8.into();
-}
+type Dictionary = HashMap<Vec<u8>, Vec<String>>;
 
 static DIGITS: [&str; 10] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
@@ -30,7 +22,11 @@ fn main() -> io::Result<()> {
 
     let mut words = Vec::new();
     for num in read_lines(input_file)?.flatten() {
-        let digits: Vec<_> = num.chars().filter(|ch| ch.is_digit(10)).collect();
+        let digits: Vec<_> = num
+            .chars()
+            .filter(|ch| ch.is_digit(10))
+            .map(|ch| ch as u8 - b'0')
+            .collect();
         print_translations(&num, &digits, 0, &mut words, &dict)?;
     }
     Ok(())
@@ -38,7 +34,7 @@ fn main() -> io::Result<()> {
 
 fn print_translations<'dict>(
     num: &str,
-    digits: &[char],
+    digits: &[u8],
     start: usize,
     words: &mut Vec<&'dict str>,
     dict: &'dict Dictionary,
@@ -47,11 +43,9 @@ fn print_translations<'dict>(
         print_solution(num, words);
         return Ok(());
     }
-    let mut n = ONE.clone();
     let mut found_word = false;
     for i in start..digits.len() {
-        n = &n * (&*TEN) + nth_digit(digits, i);
-        if let Some(found_words) = dict.get(&n) {
+        if let Some(found_words) = dict.get(&digits[start..i + 1]) {
             for word in found_words {
                 found_word = true;
                 words.push(word);
@@ -61,8 +55,7 @@ fn print_translations<'dict>(
         }
     }
     if !found_word && !words.last().map(|w| is_digit(w)).unwrap_or(false) {
-        let digit = nth_digit(digits, start);
-        words.push(DIGITS[digit]);
+        words.push(DIGITS[usize::from(digits[start])]);
         print_translations(num, digits, start + 1, words, dict)?;
         words.pop();
     }
@@ -97,23 +90,15 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn word_to_number(word: &str) -> BigUint {
-    let mut n = ONE.clone();
-    for digit in word.chars().filter_map(char_to_digit) {
-        n = &n * (&*TEN) + digit;
-    }
-    n
-}
-
-fn nth_digit(digits: &[char], i: usize) -> usize {
-    (digits[i] as usize) - ('0' as usize)
+fn word_to_number(word: &str) -> Vec<u8> {
+    word.chars().filter_map(char_to_digit).collect()
 }
 
 fn is_digit(string: &str) -> bool {
     string.len() == 1 && string.chars().next().unwrap().is_digit(10)
 }
 
-fn char_to_digit(ch: char) -> Option<u32> {
+fn char_to_digit(ch: char) -> Option<u8> {
     Some(match ch.to_ascii_lowercase() {
         'e' => 0,
         'j' | 'n' | 'q' => 1,
